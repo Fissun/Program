6155756156deb1b9ee25bc6a1d0b85ec83e78af1
Fissun <72155606+Fissun@users.noreply.github.com>
2020-09-30 20:57:50 +0300

    Add files via upload

---
+++ linear1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float a;
+    float b;
+
+    printf ("Equation tipe: x*a + b = 0\n");
+
+    printf ("Enter a: ");
+    scanf ("%f", &a);
+    printf ("Enter b: ");
+    scanf ("%f", &b);
+
+    x=-b/a;
+
+    printf ("Answer: %0.2f", x);
+
+    return (0);
+}
